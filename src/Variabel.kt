38b50fbd34d920ba9ff52a6d09bfe70402086bbe
tiara <tiaralecah@gmail.com>
2025-09-20 19:32:34 +0700

    initial commit

--- src/Variabel.kt
+++ src/Variabel.kt
@@ -1,6 +1,9 @@
 fun main(args: Array<String>) {
     var dynamicVar = 2
     println("Contoh dynamic variabel = " + dynamicVar)
-}
+
+        var staticVar: Char = 'A'
+        println("Contoh static variabel = " + staticVar)
+    }
 
 
